630c6960a4189470862cdffde7cbc3e531940de4
vagrant <vagrant@rails-dev-box>
2014-07-17 21:34:24 +0000

    added create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,33 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
+   def create
+     @post = Post.new(params.require(:post).permit(:title, :body))
+     if @post.save
+       flash[:notice] = "Se ha guardado tu entrada."
+       redirect_to @post
+     else
+       flash[:error] = "Hubo un error al guardar la entrada. Inténtalo de nuevo."
+       render :new
+     end
+   end
+
   def edit
+    @post = Post.find(params[:id])
   end
+
+
+def update
+     @post = Post.find(params[:id])
+     if @post.update_attributes(params.require(:post).permit(:title, :body))
+       flash[:notice] = "Tu entrada se ha actualizado"
+       redirect_to @post
+     else
+       flash[:error] = "Hubo un error al guardar la entrada. Inténtalo de nuevo."
+       render :edit
+     end
+   end
 end
